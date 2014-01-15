54b3b37b5e330369c34a34847195145165af3329
Javier Jaimes <sr.pasajero@lanzacohete.co>
2014-01-15 12:23:56 -0500

    Refactoring view

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.get( '/', function( req, res ){
   }else{
     db.view( 'datasets/byUser', {  key: req.user.id }, function( err, docs ){
       console.log( docs );
-      res.render( 'dashboard/index', { 'datasets': docs, 'user': req.user } );
+      res.render( 'dashboard/index', { 'thesets': docs, 'user': req.user } );
     })
   }
 })
